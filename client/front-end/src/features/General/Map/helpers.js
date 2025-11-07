a3a5570877b025c41d2f74e19960dc3c302d5a39
Tor Anders Gustavsen <tor.anders@arkitektum.no>
2025-11-07 13:03:44 +0100

    Fixed a front-end bug

--- client/front-end/src/features/General/Map/helpers.js
+++ client/front-end/src/features/General/Map/helpers.js
@@ -1,47 +1,14 @@
-import proj4 from "proj4";
-import { register } from "ol/proj/proj4";
-import GeoJSON from "ol/format/GeoJSON";
+import bbox from '@turf/bbox';
+import { getCrsName, getEpsgCode } from 'utils/helpers';
 
-export function buildQueryFromInputGeometry(inputGeometry) {
-   if (!inputGeometry || inputGeometry.type !== "MultiPolygon") {
-      throw new Error("Expected a GeoJSON-like MultiPolygon geometry.");
-   }
+export function buildQueryString(inputGeometry) {
+    if (!inputGeometry) {
+        return null;
+    }
 
-   const epsgCode = extractEpsgCodeFromCrs(inputGeometry.crs) || "4326";
-   const epsg = "EPSG:" + epsgCode;
+    const crsName = getCrsName(inputGeometry);
+    const epsgCode = getEpsgCode(crsName);
+    const extent = bbox(inputGeometry);
 
-   if (epsg === "EPSG:25833") {
-      ensureEpsg25833IsDefined();
-   } else {
-      register(proj4);
-   }
-
-   const format = new GeoJSON();
-   const geom = format.readGeometry(
-      { type: "MultiPolygon", coordinates: inputGeometry.coordinates },
-      { dataProjection: epsg, featureProjection: epsg }
-   );
-
-   let extent = geom.getExtent();
-
-   const query = `?extent=${extent.join(",")}&epsg=${epsgCode}`;
-   return query;
-}
-
-function ensureEpsg25833IsDefined() {
-   if (!proj4.defs["EPSG:25833"]) {
-      proj4.defs(
-         "EPSG:25833",
-         "+proj=utm +zone=33 +ellps=GRS80 +units=m +no_defs +type=crs"
-      );
-   }
-   register(proj4);
-}
-
-function extractEpsgCodeFromCrs(crs) {
-   if (!crs) return null;
-
-   const name = crs?.properties?.name || crs?.name || "";
-   const m = name.match(/EPSG[:]*[:]*([0-9]+)/i);
-   return m ? m[1] : null;
+    return `?extent=${extent.join(',')}&epsg=${epsgCode}`;
 }
--- client/front-end/src/features/General/Map/index.jsx
+++ client/front-end/src/features/General/Map/index.jsx
@@ -1,38 +1,50 @@
 import { Button, Paper, Tooltip } from "@mui/material";
-import { buildQueryFromInputGeometry } from "./helpers";
+import { buildQueryString } from "./helpers";
 import styles from "./Map.module.scss";
 
+const DATAFUNN_URL = import.meta.env.VITE_DATAFUNN_URL;
+
 export default function Map({ rasterResult, inputGeometry }) {
-   if (!rasterResult) {
-      return null;
-   }
+    if (!rasterResult) {
+        return null;
+    }
 
-   const url = `${
-      import.meta.env.VITE_DATAFUNN_URL
-   }/${buildQueryFromInputGeometry(inputGeometry)}`;
+    function renderDatafunnButton() {
+        const queryString = buildQueryString(inputGeometry);
 
-   return (
-      <Paper className={styles.mapImage}>
-         <div>
-            <img src={rasterResult} alt="Kartutsnitt" />
-         </div>
+        if (queryString === null) {
+            return null;
+        }
 
-         <div className={styles.buttonContainer}>
-            <Tooltip
-               slotProps={{ tooltip: { sx: { fontSize: "1em" } } }}
-               title="Mulighet for å melde inn feil eller mangler i temadata til en førstelinje behandling i en konseptutprøving i geolett 2 datafunn"
-            >
-               <Button
-                  className={styles.button}
-                  variant="contained"
-                  href={url}
-                  target="_blank"
-                  rel="noopener noreferrer"
-               >
-                  Meld feil i kartet (Test)
-               </Button>
-            </Tooltip>
-         </div>
-      </Paper>
-   );
+        const url = `${DATAFUNN_URL}/${queryString}`;
+
+        return (
+            <div className={styles.buttonContainer}>
+                <Tooltip
+                    slotProps={{ tooltip: { sx: { fontSize: "1em" } } }}
+                    title="Mulighet for å melde inn feil eller mangler i temadata til en førstelinje behandling i en konseptutprøving i geolett 2 datafunn"
+                >
+                    <Button
+                        className={styles.button}
+                        variant="contained"
+                        href={url}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Meld feil i kartet (test)
+                    </Button>
+                </Tooltip>
+            </div>
+        );
+    }
+
+    return (
+        <Paper className={styles.mapImage}>
+            <div>
+                <img src={rasterResult} alt="Kartutsnitt" />
+            </div>
+
+            {renderDatafunnButton()}
+        </Paper>
+    );
 }
