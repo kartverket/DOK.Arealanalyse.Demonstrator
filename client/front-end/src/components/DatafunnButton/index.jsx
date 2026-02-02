27fdcec7aeaa91922f13de7127354b5c4d476004
Sondre Bitubekk <sondre@arkitektum.no>
2026-02-02 16:52:22 +0100

    Revert "TEAM1-605: Call endpoint to get datafunn URL per env"
    This reverts commit 21e6fc332b0176a0a5e2073febb494db69f0c9af.

--- client/front-end/src/components/DatafunnButton/index.jsx
+++ client/front-end/src/components/DatafunnButton/index.jsx
@@ -1,26 +1,24 @@
 import { Button, Tooltip } from "@mui/material";
 import bbox from "@turf/bbox";
 import datafunn from "assets/gfx/datafunn.svg";
-import useDatafunnUrl from "hooks/useDatafunnUrl";
 import { getCrsName, getEpsgCode } from "utils/helpers";
 import styles from "./DatafunnButton.module.scss";
 
-const DATAFUNN_SYSTEM = "DOK Arealanalyse Demonstrator";
+const DATAFUNN_URL = import.meta.env.VITE_DATAFUNN_URL;
 
 export default function DatafunnButton({ className, inputGeometry }) {
-   const { datafunnUrl } = useDatafunnUrl();
-
    if (!inputGeometry) return null;
 
    const extent = bbox(inputGeometry);
 
    let queryString = `?extent=${extent.join(",")}`;
+   const systemName = "DOK Arealanalyse Demonstrator";
 
    const crsName = getCrsName(inputGeometry);
    const epsgCode = getEpsgCode(crsName);
 
    if (epsgCode) queryString += `&epsg=${epsgCode}`;
-   queryString += `&system=${encodeURIComponent(DATAFUNN_SYSTEM)}`;
+   queryString += `&system=${encodeURIComponent(systemName)}`;
 
    return (
       <div className={className}>
@@ -30,7 +28,7 @@ export default function DatafunnButton({ className, inputGeometry }) {
          >
             <Button
                variant="contained"
-               href={datafunnUrl ? `${datafunnUrl}/${queryString}` : ""}
+               href={`${DATAFUNN_URL}/${queryString}`}
                target="_blank"
                rel="noopener noreferrer"
             >
