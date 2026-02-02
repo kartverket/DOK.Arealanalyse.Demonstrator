1a0a9b8db69e35ea3a5cbd96853080f868736293
Sondre Bitubekk <sondre@arkitektum.no>
2026-02-02 16:15:54 +0100

    TEAM1-605: Added endpoint to get datafunn URL per env

---
+++ client/api/Dok.Arealanalyse.Api.Application/Models/Settings/DatafunnSettings.cs
@@ -0,0 +1,7 @@
+namespace Dok.Arealanalyse.Api.Application.Models.Settings;
+
+public class DatafunnSettings
+{
+    public static readonly string SectionName = "Datafunn";
+    public string Url { get; set; }
+}
